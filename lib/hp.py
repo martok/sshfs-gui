d68e5878e95f75117acfc0d592d18f243d42e6de
Martok <martok@martoks-place.de>
2022-01-03 00:25:53 +0100

    support: finish hexpatch implementation

--- lib/hp.py
+++ lib/hp.py
@@ -25,22 +25,16 @@ ab 00 aa bb ec 33 55
 import sys
 
 
-def main(patch, left, right=None):
-    if right is None:
-        right = left + ".patched"
-
-    with open(left, "rb") as f:
-        source = f.read()
+def read_patterns(io):
+    def dataline(it) -> str:
+        while True:
+            l = next(it).strip()
+            if l and not l.startswith('#'):
+                return l
 
     patterns = []
-    with open(patch, "rt") as f:
-        def dataline(it) -> str:
-            while True:
-                l = next(it).strip()
-                if l and not l.startswith('#'):
-                    return l
-
-        liter = iter(f)
+    liter = iter(io)
+    while True:
         try:
             l = dataline(liter)
             a = bytes.fromhex(l)
@@ -48,24 +42,42 @@ def main(patch, left, right=None):
             b = bytes.fromhex(l)
             patterns.append((a, b))
         except StopIteration:
-            pass
+            break
     patterns.sort(key=lambda p: len(p[0]), reverse=True)
+    return patterns
+
+
+def main(patch, left, right=None):
+    if right is None:
+        right = left + ".patched"
+
+    with open(left, "rb") as f:
+        source = f.read()
+
+    with open(patch, "rt") as f:
+        patterns = read_patterns(f)
+
+    print(f"Patch {patch} contains {len(patterns)} pattern(s)")
+    print(f"Original file size: {len(source)}")
 
     modified = bytearray(source)
     wp = 0
     while wp < len(modified):
         found = False
-        for pat, rep in patterns:
+        for ipat, (pat, rep) in enumerate(patterns):
             try:
                 loc = modified.index(pat, wp)
             except ValueError:
                 continue
             modified[loc:loc + len(pat)] = rep
-            wp += len(rep)
-            found = True
+            print(f"Applied patch {ipat} at position {loc}")
+            wp = loc + len(rep)
             break
-        if not found:
+        else:
+            # no further matches
             break
+
+    print(f"Patched file size: {len(modified)}")
 
     with open(right, "wb") as f:
         f.write(modified)
